a991fa41f781e484f5cc318131a81a3d01c746e1
unknown-user <unknown-user@10.211.55.2>
2014-01-14 16:47:28 +0530

    Initial check-in of module iOSSApp

---
+++ iOSSAppViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace iOSSApp
+{
+	public partial class iOSSAppViewController : UIViewController
+	{
+		public iOSSAppViewController () : base ("iOSSAppViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
